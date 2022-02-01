177248f5efe3c5ef858ef9c6d1553451a48b7c86
OS01GLADOS <no.name.tyan.d@gmail.com>
2022-02-01 12:27:27 +0300

    changed db to postgres

--- blog/database.py
+++ blog/database.py
@@ -3,9 +3,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLACHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
+DB_HOST_SERVER = 'localhost'
+DB_SERVER_PORT = '5432'
+DB_DATABASE_NAME = 'postgres'
+DB_USERNAME = 'username'
+DB_PASSWORD = 'password'
+POSTGRES_DB_URL = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST_SERVER}:{DB_SERVER_PORT}/{DB_DATABASE_NAME}'
 
-engine = create_engine(SQLACHEMY_DATABASE_URL,
-                       connect_args={'check_same_thread': False})
+
+engine = create_engine(POSTGRES_DB_URL)
 
 
 SessionLocal = sessionmaker(bind=engine,
